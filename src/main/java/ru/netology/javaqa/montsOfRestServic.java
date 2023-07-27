5b20220d58206427191056564737d68f276a335d
AndreyT <zanarions@gmail.com>
2023-07-27 16:49:52 +0300

    Add code

--- src/main/java/ru/netology/javaqa/montsOfRestServic.java
+++ src/main/java/ru/netology/javaqa/montsOfRestServic.java
@@ -1,2 +1,17 @@
-package ru.netology.javaqa;public class montsOfRestServic {
+package ru.netology.javaqa;
+
+public class montsOfRestServic {
+    public int calculate(int threshold, int income, int expense) {
+        int count = 0; // счётчик месяцев отдыха
+        int money = 0; // количество денег на счету
+        for (int month = 0; month < 12; month++) {
+            if (money >= threshold) { // можем ли отдыхать?
+                count++; // увеличиваем счётчик месяцев отдыха
+                money = (money - expense) / 3;
+            } else {
+                money = money + income - expense;
+            }
+        }
+        return count;
+    }
 }
--- src/test/java/ru/netology/javaqa/monthsOfRestServicTest.java
+++ src/test/java/ru/netology/javaqa/monthsOfRestServicTest.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class monthsOfRestServicTest {
+package ru.netology.javaqa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import ru.netology.javaqa.montsOfRestServic;
+
+public class monthsOfRestServicTest {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/Rest.csv")
+    public void testmonthsOfrRst(int expected, int threshold, int income, int expense) {
+        montsOfRestServic service = new montsOfRestServic();
+
+        int actual = service.calculate(threshold, income, expense);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
