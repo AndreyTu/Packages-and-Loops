a7520510016815f6a3c0fb85f0248f67d5953b57
AndreyT <zanarions@gmail.com>
2023-07-27 16:52:26 +0300

    Delete src/main

--- src/main/java/ru/netology/javaqa/montsOfRestServic.java
+++ src/main/java/ru/netology/javaqa/montsOfRestServic.java
@@ -2,11 +2,11 @@ package ru.netology.javaqa;
 
 public class montsOfRestServic {
     public int calculate(int threshold, int income, int expense) {
-        int count = 0; // счётчик месяцев отдыха
-        int money = 0; // количество денег на счету
+        int count = 0;
+        int money = 0;
         for (int month = 0; month < 12; month++) {
-            if (money >= threshold) { // можем ли отдыхать?
-                count++; // увеличиваем счётчик месяцев отдыха
+            if (money >= threshold) {
+                count++;
                 money = (money - expense) / 3;
             } else {
                 money = money + income - expense;
